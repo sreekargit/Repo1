20299ae89a764c6177d65064b41d6c3e8d30abc5
sreekargit <123006153+sreekargit@users.noreply.github.com>
2023-02-01 16:52:52 -0600

    Update installing-jenkinserver.sh

--- installing-jenkinserver.sh
+++ installing-jenkinserver.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+java -version
+sudo apt update
+apt install git
+apt install maven
+sudo apt install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins
